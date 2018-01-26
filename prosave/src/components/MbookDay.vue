6d544019b2eb1b7c9777f9bc00e7df27b7c489c1
groovypark <soobin950@gmail.com>
2018-01-26 17:49:57 +0900

    Update list table using v-for

--- prosave/src/components/MbookDay.vue
+++ prosave/src/components/MbookDay.vue
@@ -24,14 +24,19 @@
         <span class="main-color">총</span> ₩52,000
       </div>
     </div>
-    <div>
-      <ul class="list">
-        <li>식품</li>
-        <li>주거</li>
-        <li>쇼핑</li>
-        <li>교통</li>
-        <li>기타</li>
-      </ul>
+    <table class="list main-color">
+      <!-- 경고 : vue 자체에서 v-for 쓸때 key는 원시타입으로 바인딩 해주는걸 추천 -->
+      <tr v-for="list in spendList">
+        <td class="col1"><span v-bind:class="circle"></span>{{ list.type }}</td>
+        <td class="col2">{{ list.count }}</td>
+        <td class="col3">{{ list.total }}</td>
+      </tr>
+    </table>
+
+    <router-link to="add"><img class="btn-add" src="static/img/btnAdd.png"></router-link>
+    <div class="footer-total main-color">
+      <span class="circle4" style="font-size:2.5em"></span>
+      <span class="footer-text"> 통계</span>
     </div>
 
   </div>
@@ -42,7 +47,39 @@ export default {
   name: 'MbookDay',
   data() {
     return {
-      msg: ' ',
+      circle: ['circle1', 'circle2', 'circle3', 'circle4', 'circle5'],
+      spendList: [
+        {
+          type: '식품',
+          count: '2건',
+          total: '₩ 22,000',
+          circle: 'circle1',
+        },
+        {
+          type: '주거',
+          count: '1건',
+          total: '₩ 12,000',
+          circle: 'circle2',
+        },
+        {
+          type: '쇼핑',
+          count: '1건',
+          total: '₩ 10,000',
+          circle: 'circle3',
+        },
+        {
+          type: '교통',
+          count: '3건',
+          total: '₩ 5,000',
+          circle: 'circle4',
+        },
+        {
+          type: '기타',
+          count: '1건',
+          total: '₩ 3,000',
+          circle: 'circle5',
+        },
+      ],
     };
   },
 };
@@ -52,6 +89,7 @@ export default {
 <style scoped>
   .mBookDay {
     width: 100%;
+    height: 41.688em;
   }
   .menu-bar {
     height: 3.375em;
@@ -72,7 +110,6 @@ export default {
 
   /* The container <div> - needed to position the dropdown content */
   .dropdown {
-    float: left;
     position: relative;
     display: inline-block;
   }
@@ -121,14 +158,73 @@ export default {
     float: right;
     font-size: 0.875em;
   }
+
+   /* table css start  */
   .list {
+    width: 100%;
     list-style: none;
-    vertical-align: center;
+    font-size: 0.813em;
   }
-  .list li {
+  .list tr {
     width: 375px;
     height: 34px;
+    vertical-align: center;
     box-shadow: inset 0 -1px 0 0 #efefef;
     padding: 0.3em 0.5em 0.1em 0.5em;
+  }
+  .col1 {
+    width: 67%;
+    padding-left: 1em;
+  }
+  .col3 {
+    text-align: right;
+    padding-right: 1em;
+  }
+  .circle1:before {
+    content: ' \25CF';
+    font-size: 1.5em;
+    color: #a3a3a4;
+    padding-right: 0.2em;
+  }
+  .circle2:before {
+    content: ' \25CF';
+    font-size: 1.5em;
+    color: #363639;
+    padding-right: 0.2em;
+  }
+  .circle3:before {
+    content: ' \25CF';
+    font-size: 1.5em;
+    color: #eeeded;
+    padding-right: 0.2em;
+  }
+  .circle4:before {
+    content: ' \25CF';
+    font-size: 1.5em;
+    color: #d1d1d2;
+    padding-right: 0.2em;
+  }
+  .circle5:before {
+    content: ' \25CF';
+    font-size: 1.5em;
+    color: #747477;
+    padding-right: 0.2em;
+  }
+  /* table css end  */
+
+  .footer-total {
+    position: absolute;
+    bottom: 0em;
+    left: 1em;
+  }
+  .footer-text {
+    position: absolute;
+    top: 40%;
+    width: 1.875em;
+  }
+  .btn-add {
+    position: absolute;
+    bottom: 1.5em;
+    right: 1em;
   }
 </style>
