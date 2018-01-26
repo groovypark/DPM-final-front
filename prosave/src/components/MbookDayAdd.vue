d0bac8a724c75c77859a7ec4db5dbea414235bd9
groovypark <soobin950@gmail.com>
2018-01-26 18:22:29 +0900

    Create MbookDayAdd header

--- prosave/src/components/MbookDayAdd.vue
+++ prosave/src/components/MbookDayAdd.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="mBookDay-add">
-    
+    <div class="header">
+      <div class="title">
+        지출
+        <router-link to="/"><span class="close">x</span></router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,5 +22,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.header {
+  width: 100%;
+  height: 11.313em;
+  background-color: #efefef;
+}
+.title {
+  padding: 1.2em;
+  font-size: 1em;
+}
+.close {
+  float: right;
+  color: var(--gray);
+  font-size: 1.3em;
+  margin-right: 0.2em;
+}
 </style>
