9f6d3189ad0cdbf45393fc66558d95e3572f6a2e
groovypark <soobin950@gmail.com>
2018-01-26 17:50:20 +0900

    Add MbookDayAdd route path

--- prosave/src/router/index.js
+++ prosave/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import MbookDay from '@/components/MbookDay';
+import MbookDayAdd from '@/components/MbookDayAdd';
 // import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router);
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'MbookDay',
       component: MbookDay,
+    },
+    {
+      path: '/add',
+      name: 'MbookDayAdd',
+      component: MbookDayAdd,
     },
   ],
 });
